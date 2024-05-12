ad9ceeacbcc3ef6e952db09619effd4fa4119253
Talha Mujahid <htmujahid@gmail.com>
2024-05-12 10:18:55 +0500

    feat: version update

--- src/App.ts
+++ src/App.ts
@@ -1,6 +1,6 @@
 import { $component, SonnetComponent } from '@sonnetjs/core';
-import { HeroSection } from './components/sections';
 import { Header, Footer } from './components/shared';
+import { HeroSection, TextSection } from './components/sections';
 
 class App extends SonnetComponent {
   public get() {
@@ -9,6 +9,10 @@ class App extends SonnetComponent {
         ${Header()}
         <main>
           ${HeroSection().get()}
+          ${TextSection().children("No Extra Runtime Compiler Required").get()}
+          ${TextSection().children("JavaScript Framework for Structured Code").get()}
+          ${TextSection().children("As fast as Native JavaScript").get()}
+          ${TextSection().children("Composable and Reusable Components").get()}
         </main>
         ${Footer()}
       </div>
