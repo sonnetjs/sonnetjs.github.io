8ecbcbb21fc7983bb3b134fa0f17dbba2aad4c7c
htmujahid <htmujahid@gmail.com>
2024-06-18 06:54:36 +0500

    feat: normalize css

--- .docusaurus/client-modules.js
+++ .docusaurus/client-modules.js
@@ -1,6 +1,6 @@
 export default [
-  require("/Users/apple/Documents/projects/sonnetjs.github.io/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/apple/Documents/projects/sonnetjs.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/apple/Documents/projects/sonnetjs.github.io/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/apple/Documents/projects/sonnetjs.github.io/src/css/custom.css"),
+  require("D:\\Projects\\sonnetjs\\sonnetjs.github.io\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("D:\\Projects\\sonnetjs\\sonnetjs.github.io\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("D:\\Projects\\sonnetjs\\sonnetjs.github.io\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("D:\\Projects\\sonnetjs\\sonnetjs.github.io\\src\\css\\custom.css"),
 ];
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ['class'],
-  content: [
-    './src/**/*.{ts,tsx}',
-  ],
+  content: ['./src/**/*.{ts,tsx}'],
   prefix: '',
   theme: {
     container: {
@@ -92,5 +90,8 @@ module.exports = {
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  corePlugins: {
+    preflight: false,
+  },
+  plugins: [require('@tailwindcss/typography'), require('tailwindcss-animate')],
 };
